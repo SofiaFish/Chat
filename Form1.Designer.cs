0a5ae4be77c97a3000840bc37fb40afbd242813d
SofiaFish <37893374+SofiaFish@users.noreply.github.com>
2019-06-04 14:00:17 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbMessage = new System.Windows.Forms.TextBox();
+            this.bSend = new System.Windows.Forms.Button();
+            this.lbChat = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbMessage
+            // 
+            this.tbMessage.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbMessage.Font = new System.Drawing.Font("Myriad Pro", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbMessage.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.tbMessage.Location = new System.Drawing.Point(17, 360);
+            this.tbMessage.Multiline = true;
+            this.tbMessage.Name = "tbMessage";
+            this.tbMessage.Size = new System.Drawing.Size(513, 45);
+            this.tbMessage.TabIndex = 0;
+            this.tbMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbMessage_KeyDown);
+            // 
+            // bSend
+            // 
+            this.bSend.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.bSend.Font = new System.Drawing.Font("Myriad Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bSend.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.bSend.Location = new System.Drawing.Point(541, 358);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(79, 50);
+            this.bSend.TabIndex = 1;
+            this.bSend.Text = "Send";
+            this.bSend.UseVisualStyleBackColor = false;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            // 
+            // lbChat
+            // 
+            this.lbChat.BackColor = System.Drawing.Color.Black;
+            this.lbChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbChat.Font = new System.Drawing.Font("Myriad Pro", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbChat.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.lbChat.FormattingEnabled = true;
+            this.lbChat.ItemHeight = 16;
+            this.lbChat.Location = new System.Drawing.Point(16, 18);
+            this.lbChat.Name = "lbChat";
+            this.lbChat.Size = new System.Drawing.Size(515, 304);
+            this.lbChat.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(623, 425);
+            this.Controls.Add(this.lbChat);
+            this.Controls.Add(this.bSend);
+            this.Controls.Add(this.tbMessage);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Chatnik";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMessage;
+        private System.Windows.Forms.Button bSend;
+        private System.Windows.Forms.ListBox lbChat;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            IPAddress address = IPAddress.Parse("127.0.0.1");
+            IPEndPoint endPoint = new IPEndPoint(address, 22000);
+            TcpClient client = new TcpClient();
+            //Console.WriteLine("Нажмите кнопку чтобы подключиться");
+            //Console.ReadKey();
+            client.Connect(endPoint);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            while (true)
+            {
+                string text = Console.ReadLine();
+                DataLayer.Message message = new DataLayer.Message
+                {
+                    Text = text,
+                    Date = DateTime.Now
+                };
+                NetworkStream networkStream = client.GetStream();
+                formatter.Serialize(networkStream, message);
+                if (text == "exit")
+                    break;
+            }
+            client.Close();
+        }
+
+        private void bSend_Click(object sender, EventArgs e)
+        {
+            lbChat.Items.Add(tbMessage.Text);
+            tbMessage.Clear();
+        }
+        
+        private void tbMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {
+                lbChat.Items.Add(tbMessage.Text);
+                tbMessage.Clear();
+            }
+        }
+    }
+}
